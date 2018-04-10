fc090196428911fc9888237ffb756caad6444127
Maciej Sławik <maciej.slawik@lizardmedia.pl>
2018-04-10 09:36:04 +0200

    Adding keystore events for turning pages

--- view/frontend/web/js/MSlwk.iCatalogue.js
+++ view/frontend/web/js/MSlwk.iCatalogue.js
@@ -7,15 +7,20 @@ define(
 
         var init;
 
-        init = function (config) {
-            $('.flipbook').turn({
-                width: config.width,
+        init = function (config, element) {
+            $(element).turn({
+                width: cfig.width,
                 height: config.height,
                 elevation: 50,
                 gradients: true,
                 autoCenter: false
             });
-
+            $(window).bind('keydown', function (e) {
+                if (e.keyCode === 37)
+                    $(element).turn('previous');
+                else if (e.keyCode === 39)
+                    $(element).turn('next');
+            });
         };
 
         return init;
